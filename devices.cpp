c36ef77d4dc06500d903037c94db6f6af5af8474
danieldkato <daniel.d.kato@gmail.com>
2016-06-13 15:37:55 -0400

    Edit comments for devices.cpp

--- devices.cpp
+++ devices.cpp
@@ -1,23 +1,46 @@
-/* Source file with device class definitions. All device
- * classes inherit from the parent class "Device", and 
- * include an updateDev function that takes 4 arguments:
+/*  Last updated DDK 6/7/16
  * 
- * 1) an integer index indicating (via conditional
- * statement) which one of a discrete number of actions 
- * the device should perform on the current trial,
+ * OVERVIEW:
+ * This file contains source code for the library `devices`, used for controlling
+ * Arduino-managed devices for `ArduFSM` protocols like `MultiSens`.
+ *
+ *
+ * REQUIREMENTS: 
+ * This file should be in the same directory as its header file `devices.h`. In 
+ * addition, the Arduino sketchbook's library folder should contain the following
+ * libraries:
  * 
- * 2) the time (relative to trial start) to begin the
- * action,
+ * * `Arduino`
+ * * `Stepper`
+ * * `Tone`
  * 
- * 3) the time (relative to trials start) to end the action,
- * and 
  * 
- * 4) the trial start time. 
+ * DESCRIPTION:
+ * This library defines a number of devices classes, each of which 
+ * corresponds to a type of device that might be controlled from 
+ * an Arduino - e.g., steppers, speakers, solenoids, etc. 
  * 
- * The devices to be used in a given experiment are
- * instantiated in a function defined in config.h and
- * config.cpp, called from the main sketch
- * multisens_hw_control.ino 
+ * Each class includes a `loop` function and an `s_finish` function. 
+ * `loop` passes an integer index to a dispatch table that specifies 
+ * some action the device should perform. For example, if a `Speaker` 
+ * object is given an index of `0`, its `loop` function may do nothing; 
+ * if it is given an index of `1`, it may generate a random tone. 
+ *
+ * `s_finish` specifies how the device should behave at the end of a 
+ * stimulus period. This includes things like turning off all speakers, 
+ * retracting all steppers, etc. 
+ * 
+ * Because all of these device classes inherit from the same parent class 
+ * (`Device`), all instances can be placed in an array, then some referring 
+ * program can iterate through the array and call each device's `loop` function 
+ * throughout the duration of some stimulus period. In practice, the `MultiSens` 
+ * protocol's `States.cpp` defines a stimulus-period object that iterates through 
+ * such an array throughout the duration of the stimulus period. The `ArduFSM` 
+ * program can thus update what each device should be doing on every pass of its 
+ * state-dependent operations, allowing for near-simultaneous control of multiple 
+ * devices throughout the duration of some stimulus period.  
+ * 
+ * For specific index-behavior mappings, see this library's `README.md` file.
  */
 
 #include "Arduino.h"
