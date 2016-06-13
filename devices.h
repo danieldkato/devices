a0c56cc8a085b85a476e648a964ff30ed6d217f2
danieldkato <daniel.d.kato@gmail.com>
2016-06-13 15:00:46 -0400

    Fix indentations

--- devices.h
+++ devices.h
@@ -29,19 +29,19 @@
 
 //Parent device declarations*******************************
 class Device{
-	public:
-		Device();
-		virtual void loop( int fcnIdx ) =0;
-    virtual void s_finish() =0;
+    public:
+        Device();
+        virtual void loop( int fcnIdx ) =0;
+        virtual void s_finish() =0;
     
-    virtual void ping() =0;
-    //virtual void ping();
-    //void ping();
+        virtual void ping() =0;
+        //virtual void ping();
+        //void ping();
   
-  private:
-    static int deviceCounter;
-    static String devBaseName;
-    String devID;
+    private:
+        static int deviceCounter;
+        static String devBaseName;
+        String devID;
 };
 
 
@@ -49,21 +49,21 @@ class Device{
 
 //Dummy stepper declarations
 class dummyStepper: public Device {
-	public:
-    dummyStepper();
-		void loop( int fcnIdx );
-    void s_finish();
-    void ping();
-    String stprState; //a flag to keep track of whether the stepper is extended or retracted
+    public:
+        dummyStepper();
+        void loop( int fcnIdx );
+        void s_finish();
+        void ping();
+        String stprState; //a flag to keep track of whether the stepper is extended or retracted
 };
 
 
 //Dummy speaker declarations
 class dummySpeaker: public Device {
-	public:
-		void loop( int fcnIdx );
-    void s_finish();
-    void ping();
+    public:
+        void loop( int fcnIdx );
+        void s_finish();
+        void ping();
 };
 
 
@@ -71,14 +71,14 @@ class dummySpeaker: public Device {
 
 // Speaker declarations
 class mySpeaker: public Device{
-	public:
-		mySpeaker( int pin );
-		void loop( int fcnIdx );
-    void s_finish();
-    void ping();
+    public:
+        mySpeaker( int pin );
+        void loop( int fcnIdx );
+        void s_finish();
+        void ping();
   
-	private:
-		Tone myTones[1]; 
+    private:
+        Tone myTones[1]; 
 };
 
 
@@ -87,65 +87,65 @@ stepper library plus my standard updateDev function, some
 higher-level "extend" and "retract" function and related
 variables, and hall effect sensor variables. */
 class myStepper: public Device {
-	public:
-		//constructor
-		 myStepper(int number_of_steps, int motor_pin_1, int motor_pin_2, int enbl, int hallPin, int hallThresh, int stprSpeed, int CW, int CCW, int hallVal);
+    public:
+        //constructor
+        myStepper(int number_of_steps, int motor_pin_1, int motor_pin_2, int enbl, int hallPin, int hallThresh, int stprSpeed, int CW, int CCW, int hallVal);
 
-    /*DK 151208: my update function that I need to define 
-    in every device*/
-    void loop( int fcnIdx );
-    void s_finish();
+        /*DK 151208: my update function that I need to define 
+            in every device*/
+        void loop( int fcnIdx );
+        void s_finish();
 
-    /*DK 151208: ping function just to check that device
-    has been instantiated and main sketch can call 
-    functions*/
-    void ping();
+        /*DK 151208: ping function just to check that device
+            has been instantiated and main sketch can call 
+            functions*/
+        void ping();
 
-    /*DK 151208: Flag for keeping track of whether stepper 
-    is extended or retracted */
-    String stprState;
+        /*DK 151208: Flag for keeping track of whether stepper 
+            is extended or retracted */
+        String stprState;
     
-		// speed setter method:
-		void setSpeed(long whatSpeed);
-	
-		// mover method:
-		void step(int number_of_steps);
-	
-		int version(void);
+        // speed setter method:
+        void setSpeed(long whatSpeed);
+    
+        // mover method:
+        void step(int number_of_steps);
+    
+        int version(void);
 
-	
-	private:
-		void stepMotor(int this_step);
-	
-		/*DK 151208: my extend and retract functions (taken 
+    
+    private:
+        void stepMotor(int this_step);
+    
+        /*DK 151208: my extend and retract functions (taken 
     from  Clay) that will be called from the updateDev 
     function */
-		void fwd();
-		void back();
+        void fwd();
+        void back();
 
     //DK 151208: hall effect sensor variables 
-    int enbl;
-    int hallPin;
-    int hallThresh;
-    int hallVal;
-    int stprSpeed;
-    int CW;
-    int CCW;
+        int enbl;
+        int hallPin;
+        int hallThresh;
+        int hallVal;
+        int stprSpeed;
+        int CW;
+        int CCW;
 
-		int direction;            // Direction of rotation
-		unsigned long step_delay; // delay between steps, in ms, based on speed
-		int number_of_steps;      // total number of steps this motor can take
-		int pin_count;            // how many pins are in use.
-		int step_number;          // which step the motor is on
+        int direction;            // Direction of rotation
+        unsigned long step_delay; // delay between steps, in ms, based on speed
+        int number_of_steps;      // total number of steps this motor can take
+        int pin_count;            // how many pins are in use.
+        int step_number;          // which step the motor is on
 
-		// motor pin numbers:
-		int motor_pin_1;
-		int motor_pin_2;
-		int motor_pin_3;
-		int motor_pin_4;
-		int motor_pin_5;          // Only 5 phase motor
+        // motor pin numbers:
+        int motor_pin_1;
+        int motor_pin_2;
+        int motor_pin_3;
+        int motor_pin_4;
+        int motor_pin_5;          // Only 5 phase motor
 
-		unsigned long last_step_time; // time stamp in us of when the last step was taken
+        unsigned long last_step_time; // time stamp in us of when the last step was taken
 };
 
 
